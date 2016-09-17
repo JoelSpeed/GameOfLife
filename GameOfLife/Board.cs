13aa6e386d563cf44b70646a6548c277cd6eca22
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 15:47:28 +0100

    Add overload to create next gen board from previous board

--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace GameOfLife
 {
@@ -10,7 +11,7 @@ namespace GameOfLife
         public Board(int Dimension)
         {
             this.Dimension = Dimension;
-            this.Grid = new Cell[this.Dimension,this.Dimension];
+            this.Grid = new Cell[this.Dimension, this.Dimension];
             InitialiseGrid();
         }
 
@@ -20,6 +21,14 @@ namespace GameOfLife
             this.Grid = new Cell[this.Dimension, this.Dimension];
             InitialiseGrid();
             PopulateGrid(random);
+        }
+
+        public Board(Board previousBoard)
+        {
+            this.Dimension = previousBoard.Dimension;
+            this.Grid = new Cell[this.Dimension, this.Dimension];
+            InitialiseGrid();
+            PopulateGrid(previousBoard);
         }
 
         private void InitialiseGrid()
@@ -38,13 +47,110 @@ namespace GameOfLife
         {
             int numAliveCells = random.Next(0, Dimension*Dimension);
 
-            for(int i = 0; i < numAliveCells; i++)
+            for (int i = 0; i < numAliveCells; i++)
             {
                 int row = random.Next(0, Dimension);
                 int column = random.Next(0, Dimension);
 
                 Grid[row, column].IsAlive = true;
             }
+        }
+
+        private void PopulateGrid(Board previousBoard)
+        {
+            for (int i = 0; i < Dimension; i++)
+            {
+                for (int j = 0; i < Dimension; i++)
+                {
+                    int aliveNeighbourCount = CountCellAliveNeighbours(previousBoard, i, j);
+                    Cell cell = this.Grid[i, j];
+
+                    var rule = CalculateMatchingRule(cell, aliveNeighbourCount);
+
+                    UpdateCell(rule, cell);
+
+                }
+            }
+        }
+
+        private static void UpdateCell(int rule, Cell cell)
+        {
+            switch (rule)
+            {
+                case 1:
+                    cell.IsAlive = false;
+                    break;
+                case 2:
+                    cell.IsAlive = true;
+                    break;
+                case 3:
+                    cell.IsAlive = false;
+                    break;
+                case 4:
+                    cell.IsAlive = true;
+                    break;
+                default:
+                    throw new Exception("A rule was not matched, a rule should always be matched");
+            }
+        }
+
+        private static int CalculateMatchingRule(Cell cell, int aliveNeighbourCount)
+        {
+                    /*
+                     * Rules of the game:
+                     * 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+                     * 2. Any live cell with two or three live neighbours lives on to the next generation.
+                     * 3. Any live cell with more than three live neighbours dies, as if by over - population.
+                     * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                     */
+
+            int rule = 0;
+            if (cell.IsAlive)
+            {
+                if (aliveNeighbourCount < 2)
+                    rule = 1;
+                if (aliveNeighbourCount == 2 || aliveNeighbourCount == 3)
+                    rule = 2;
+                if (aliveNeighbourCount > 3)
+                    rule = 3;
+            }
+            else
+            {
+                if (aliveNeighbourCount == 3)
+                    rule = 4;
+            }
+            return rule;
+        }
+
+        private int CountCellAliveNeighbours(Board previousBoard, int row, int column)
+        {
+            int count = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    bool neighbourIsAlive = false;
+
+                    try
+                    {
+                        neighbourIsAlive = previousBoard.Grid[row + i, column + j].IsAlive;
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        neighbourIsAlive = false;
+                    }
+                    finally
+                    {
+                        if(neighbourIsAlive)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
         }
 
         public void Print()
