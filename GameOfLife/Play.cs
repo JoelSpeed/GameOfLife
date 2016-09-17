3b2c8e98a79baa41662cb37900b13797dd8285a5
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 16:49:45 +0100

    Get play to iterate over generations

--- GameOfLife/Play.cs
+++ GameOfLife/Play.cs
@@ -9,10 +9,16 @@ namespace GameOfLife
             int Generations = 10;
             Random random = new Random(Guid.NewGuid().GetHashCode());
 
-            for (int i = 1; i <= Generations; i++)
+            Board oldBoard = new Board(10, random);
+
+            Console.WriteLine("Generation 1");
+            oldBoard.Print();
+
+            for (int i = 2; i <= Generations; i++)
             {
                 Console.WriteLine("Generation {0}", i);
-                var board = new Board(10, random);
+                var board = new Board(oldBoard);
+                oldBoard = board;
                 board.Print();
             }
 
