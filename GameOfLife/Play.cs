968c7bc8a47fa283cf04e001cfbd2368a34eadaa
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 12:38:28 +0100

    Add class Play which says Hello

---
+++ GameOfLife/Play.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Play
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World.");
+
+            // Wait for a key press before exiting
+            Console.Read();
+        }
+    }
+}
