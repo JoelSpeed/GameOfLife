6394749ce67513fa7b40a057a97a653fefc7b792
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 13:23:42 +0100

    Set up testing environment

---
+++ GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,19 @@
+﻿using ApprovalTests;
+using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+        [Test]
+        public void CheckNewCellNotAlive()
+        {
+            var cell = new Cell();
+            if (cell.IsAlive)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
