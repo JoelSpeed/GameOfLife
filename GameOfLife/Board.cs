4c9a36db98cb118ab4b1e99c579a9d985bc4c6da
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 14:08:30 +0100

    Add grid initialisation

--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -11,6 +11,19 @@ namespace GameOfLife
         {
             this.Dimension = Dimension;
             this.Grid = new Cell[this.Dimension,this.Dimension];
+            InitialiseGrid();
+        }
+
+        private void InitialiseGrid()
+        {
+            // fill the grid with new cells
+            for (int i = 0; i < Dimension; i++)
+            {
+                for (int j = 0; j < Dimension; j++)
+                {
+                    Grid[i, j] = new Cell();
+                }
+            }
         }
     }
 }
