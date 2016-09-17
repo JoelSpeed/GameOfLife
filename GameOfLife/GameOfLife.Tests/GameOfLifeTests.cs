302193a6bd8e8cab510298964764222de231f1ab
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 13:39:06 +0100

    Add tests to check board properties

--- GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
+++ GameOfLife/GameOfLife.Tests/GameOfLifeTests.cs
@@ -15,5 +15,31 @@ namespace GameOfLife.Tests
                 Assert.Fail();
             }
         }
+
+        [Test]
+        public void CheckBoardDimensionGetsSet()
+        {
+            var board = new Board(10);
+            if (board.Dimension != 10)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void CheckBoardGridIsCorrectSize()
+        {
+            var board = new Board(10);
+            // check height
+            if (board.Grid.GetLength(0) != 10)
+            {
+                Assert.Fail();
+            }
+            // check width
+            if (board.Grid.GetLength(1) != 10)
+            {
+                Assert.Fail();
+            }
+        }
     }
 }
