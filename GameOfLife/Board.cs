0d3ac21f15e2dd016d5c7df37fb56c1e69484f44
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 14:52:15 +0100

    Space makes it easier to see

--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -77,7 +77,7 @@ namespace GameOfLife
                 }
                 else
                 {
-                    row += "X";
+                    row += " ";
                 }
             }
 
