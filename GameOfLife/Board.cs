bd847606854579dca4d74e1baa86439523fb437d
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 17:19:14 +0100

    Commenting throughout + R#

--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -32,7 +32,10 @@ namespace GameOfLife
 
         private void InitialiseGrid()
         {
-            // fill the grid with new cells
+            /*
+             * Fills the boards grid with new Cell objects
+             */
+
             for (int i = 0; i < Dimension; i++)
             {
                 for (int j = 0; j < Dimension; j++)
@@ -44,6 +47,9 @@ namespace GameOfLife
 
         private void PopulateGrid(Random random)
         {
+            /* 
+             * Takes a random seed and generates a random layout on the board.
+             */
             int numAliveCells = random.Next(Dimension, Dimension*Dimension);
 
             for (int i = 0; i < numAliveCells; i++)
@@ -57,6 +63,12 @@ namespace GameOfLife
 
         private void PopulateGrid(Board previousBoard)
         {
+            /* Populates the cells of the grid based on a previous board.
+             * Each cell is evaluated against the rules of the game of life
+             * and the new board becomes the 'next generation' of the previous 
+             * board.
+             */
+
             for (int i = 0; i < Dimension; i++)
             {
                 for (int j = 0; j < Dimension; j++)
@@ -77,6 +89,11 @@ namespace GameOfLife
 
         private static void UpdateCell(int rule, Cell cell)
         {
+            /*
+             * Takes the cell from the current board and the matching rule 
+             * ID, then sets the cell to be alive or dead accordingly
+             */
+
             switch (rule)
             {
                 case 1:
@@ -98,13 +115,19 @@ namespace GameOfLife
 
         private static int CalculateMatchingRule(Cell cell, int aliveNeighbourCount)
         {
-                    /*
-                     * Rules of the game:
-                     * 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-                     * 2. Any live cell with two or three live neighbours lives on to the next generation.
-                     * 3. Any live cell with more than three live neighbours dies, as if by over - population.
-                     * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-                     */
+            /*
+             * Takes a cell from the current board and the number of alive neghbours
+             * that location had on the previous board, then process the game
+             * of life rules and returns the ID of the matching rule
+             */
+
+            /*
+             * Rules of the game:
+             * 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+             * 2. Any live cell with two or three live neighbours lives on to the next generation.
+             * 3. Any live cell with more than three live neighbours dies, as if by over - population.
+             * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+             */
 
             int rule = 0;
             if (cell.IsAlive)
@@ -124,8 +147,13 @@ namespace GameOfLife
             return rule;
         }
 
-        private int CountCellAliveNeighbours(Board previousBoard, int row, int column)
+        private int CountCellAliveNeighbours(Board board, int row, int column)
         {
+            /*
+             * Takes a board and calculates how many neighbours of a given
+             * cell are alive. 
+             */
+
             int count = 0;
 
             for (int i = -1; i <= 1; i++)
@@ -139,7 +167,7 @@ namespace GameOfLife
                         // dont count cell as a neighbour of itself
                         if (!(i == 0 && j == 0))
                         {
-                            neighbourIsAlive = previousBoard.Grid[row + i, column + j].IsAlive;
+                            neighbourIsAlive = board.Grid[row + i, column + j].IsAlive;
                         }
                     }
                     catch (IndexOutOfRangeException)
@@ -161,6 +189,10 @@ namespace GameOfLife
 
         public void Print()
         {
+            /*
+             * Prints a view of the current board where O represents
+             * an alive cell and blank space represents a dead cell.
+             */
             var horizontalBorder = CreateHorizontalBorder();
 
             // print top border
@@ -179,6 +211,8 @@ namespace GameOfLife
 
         private string ConstructRowString(int rowNumber)
         {
+            // construct a string to be printed as one line of the board
+
             string row = "|";
 
             for (int i = 0; i < Dimension; i++)
