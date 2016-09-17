b8d30db3208302a5a64a4e2eb4a90523a439e194
JoelSpeed <Joel.speed@hotmail.co.uk>
2016-09-17 13:29:48 +0100

    Add initial board class
    Intialises grid and dimension properties

---
+++ GameOfLife/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    public class Board
+    {
+        public int Dimension { get; set; }
+        public Cell[,] Grid { get; }
+
+        public Board(int Dimension)
+        {
+            this.Dimension = Dimension;
+            this.Grid = new Cell[this.Dimension,this.Dimension];
+        }
+    }
+}
